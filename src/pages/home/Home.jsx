a26084f6657567ac3d999abedbff0e7b56921ad6
juanfepy <villamizar.juan@correounivalle.edu.co>
2024-11-11 21:19:14 -0500

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,41 +4,18 @@ import "./Home.css";
 
 const Home = () => {
     return (
-    <>
-        <Header />
-            <div className="container-home">
-            <h1>¿Sabias que...?</h1>
+        <>
+            <Header />
+            
 
-        <p>
-            ♦ El agua es un recurso vital para la vida en nuestro planeta, pero enfrenta numerosos desafíos medioambientales que amenazan su disponibilidad y calidad. Estos problemas afectan tanto a los ecosistemas acuáticos como a las poblaciones humanas que dependen de este recurso esencial.
-            Siendo este impactado por diversas actividades humanas como los son las actividades industriales, el uso excesivo de pesticidas y fertilizanntes
-            la gestion inadecuada de residuos y los procesos agricolas por lo tanto, cada dia que pasa, mas fuentes de agua se encuentran contaminadas. 
-        </p>
-
-        <p>
-            ♦ Algunos de los principales problemas medioambientales relacionados con el agua incluyen:
-        </p>
-        <p1>
-            1. Contaminacion de agua
-        </p1>
-        
-        <p1>
-            2. Escazes de agua 
-        </p1>
-        <p1>
-            3. Acidficicacion de oceanos
-        </p1>
-
-        <p>
-        ♦ Afortunadamente, existen soluciones que se están implementando y desarrollando: Tales como la implementacion de tecnologias de ahorro y reutilizacion de agua,
-        educacio y concentizacion sobre el agua entre otras. 
-        </p>
-    
-    
-        </div>
-    </>
-
+                {/* Renderizando el cubo 3D */}
+                <div className="cube-container">
+                    <Cube3D />
+                </div>
+            
+        </>
     );
 };
 
 export default Home;
+
